ca2a02147b77c77c14a8664acd898e7448294b36
Guy Moore <guymoore8@gmail.com>
2024-03-19 18:17:08 +0200

    added isEqual and getZeroCoords functions to Board

--- src/Board.java
+++ src/Board.java
@@ -3,17 +3,20 @@ package src;
 import java.util.Arrays;
 
 public class Board {
-    private int size;
-    private int[][] tiles;
+    private final int size;
+    private final int[][] tiles;
+    private final int[] zeroCoords;
 
-    Board(int size) {
+    public Board(int size) {
         this.size = size;
         this.tiles = new int[size][size];
+        this.zeroCoords = getZeroCoords(tiles);
     }
 
     Board(int[][] tiles) {
         this.size = tiles.length;
         this.tiles = tiles;
+        this.zeroCoords = getZeroCoords(tiles);
     }
 
     public int getSize() {
@@ -28,5 +31,32 @@ public class Board {
         for (int[] row: tiles) {
             System.out.println(Arrays.toString(row));
         }
+        System.out.println();
+    }
+
+    public boolean isEqual(Board b) {
+        if (b.size != this.size) return false;
+        int[][] compTiles = b.getTiles();
+
+        for (int row = 0; row < this.tiles.length; row++) {
+            for (int col = 0; col < this.tiles[row].length; col++) {
+                if (this.tiles[row][col] != compTiles[row][col]) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int[] getZeroCoords() {
+        return this.zeroCoords;
+    }
+
+    static int[] getZeroCoords(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][i] == 0) return new int[]{i, j};
+            }
+        }
+        return new int[]{-1, -1};
     }
 }
