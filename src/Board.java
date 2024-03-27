ba41a8713ad57d41a032d1088a0f7348fdb3cebf
Guy Moore <guymoore8@gmail.com>
2024-03-28 01:41:26 +0200

    remade Board class

--- src/Board.java
+++ src/Board.java
@@ -1,30 +1,109 @@
 package src;
 
+import src.Algorithm.Direction;
+
 import java.util.Arrays;
 
+import static src.Util.cloneMatrix;
+
 public class Board {
-    private final int size;
-    private final int[][] tiles;
-    private final int[] zeroCoords;
+    private int[][] tiles;
+    private int zeroCol;
+    private int zeroRow;
 
-    public Board(int size) {
-        this.size = size;
-        this.tiles = new int[size][size];
-        this.zeroCoords = getZeroCoords(tiles);
-    }
+    private int depth;
 
-    Board(int[][] tiles) {
-        this.size = tiles.length;
+    private Board parent;
+
+    public Board(int[][] tiles, int zeroRow, int zeroCol) {
         this.tiles = tiles;
-        this.zeroCoords = getZeroCoords(tiles);
+        this.zeroCol = zeroCol;
+        this.zeroRow = zeroRow;
+        this.parent = null;
+        this.depth = 0;
     }
 
-    public int getSize() {
-        return this.size;
+    public Board(Board board, Direction dir) {
+        this.tiles = cloneMatrix(board.getTiles());
+        this.zeroRow = board.zeroRow;
+        this.zeroCol = board.zeroCol;
+        this.depth = board.depth;
+        this.parent = board;
+        this.moveZero(dir);
+        this.depth++;
     }
 
     public int[][] getTiles() {
         return this.tiles;
+    }
+
+    public long getDepth() {
+        return this.depth;
+    }
+
+    public Board getParent() {
+        return this.parent;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (!(obj instanceof Board other)) return false;
+
+        int[][] otherTiles = other.getTiles();
+
+        for (int row = 0; row < this.tiles.length; row++) {
+            for (int col = 0; col < this.tiles[row].length; col++) {
+                if (this.tiles[row][col] != otherTiles[row][col]) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean moveZero(Direction dir) {
+
+        switch (dir) {
+            case UP -> {
+                if (zeroRow > 0) {
+                    tiles[zeroRow][zeroCol] = tiles[zeroRow - 1][zeroCol];
+                    zeroRow--;
+                    tiles[zeroRow][zeroCol] = 0;
+                    return true;
+                }
+            }
+            case DOWN -> {
+                if (zeroRow < (tiles.length -1)) {
+                    tiles[zeroRow][zeroCol] = tiles[zeroRow + 1][zeroCol];
+                    zeroRow++;
+                    tiles[zeroRow][zeroCol] = 0;
+                    return true;
+                }
+            }
+            case LEFT -> {
+                if (zeroCol > 0) {
+                    tiles[zeroRow][zeroCol] = tiles[zeroRow][zeroCol - 1];
+                    zeroCol--;
+                    tiles[zeroRow][zeroCol] = 0;
+                    return true;
+                }
+            }
+            case RIGHT -> {
+                if (zeroCol < tiles.length - 1) {
+                    tiles[zeroRow][zeroCol] = tiles[zeroRow][zeroCol + 1];
+                    zeroCol++;
+                    tiles[zeroRow][zeroCol] = 0;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Board clone() {
+        return new Board(cloneMatrix(tiles), zeroRow, zeroCol);
     }
 
     public void printBoard() {
@@ -32,31 +111,5 @@ public class Board {
             System.out.println(Arrays.toString(row));
         }
         System.out.println();
-    }
-
-    public boolean isEqual(Board b) {
-        if (b.size != this.size) return false;
-        int[][] compTiles = b.getTiles();
-
-        for (int row = 0; row < this.tiles.length; row++) {
-            for (int col = 0; col < this.tiles[row].length; col++) {
-                if (this.tiles[row][col] != compTiles[row][col]) return false;
-            }
-        }
-
-        return true;
-    }
-
-    public int[] getZeroCoords() {
-        return this.zeroCoords;
-    }
-
-    static int[] getZeroCoords(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][i] == 0) return new int[]{i, j};
-            }
-        }
-        return new int[]{-1, -1};
     }
 }
