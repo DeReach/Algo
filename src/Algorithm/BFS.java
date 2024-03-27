bc83c27bf0a734ab76c4d559f198d51148ded1fa
Guy Moore <guymoore8@gmail.com>
2024-03-28 01:41:57 +0200

    use utils to generate a random board

--- src/Algorithm/BFS.java
+++ src/Algorithm/BFS.java
@@ -26,7 +26,6 @@ public class BFS {
         startTime = System.nanoTime();
 
         while (currentBoard != null && !currentBoard.equals(solvedBoard)) {
-//            currentBoard.printBoard();
             System.out.println(nodesExpanded);
             if (!visitedList.contains(currentBoard)) {
                 for (Direction d : Direction.values()) {
@@ -40,44 +39,12 @@ public class BFS {
         endTime = System.nanoTime();
         totalTime = endTime - startTime;
         if (currentBoard == null) {
-            System.out.println("Could not find solution after expanding " + nodesExpanded + " nodes. Searched for " + totalTime * 1E-6 + "ms.");
+            System.out.println("Could not find solution after creating " + nodesExpanded + " vertices. Searched for " + totalTime * 1E-6 + "ms.");
         } else {
 //                Display out = new Display();
-            System.out.println("Solution found after expanding " + nodesExpanded + " nodes. Searched for " + totalTime * 1E-6 + "ms. Depth: " + currentBoard.getDepth());
+            System.out.println("Solution found after creating " + nodesExpanded + " vertices. Searched for " + totalTime * 1E-6 + "ms. Moves: " + currentBoard.getDepth());
 //                System.out.println(out.solutionToString(currentBoard));
         }
 
     }
 }
-//        Graph graph = new Graph(initialBoard);
-//        Queue<Integer> queue = new LinkedList<>();
-//        queue.add(0);
-//
-//        while(!queue.isEmpty()) {
-//            Vertex currentVertex = graph.getVertex(queue.poll());
-////            Board currentBoard = currentVertex.getBoard();
-//            currentBoard.printBoard();
-//            if (currentBoard.equals(solvedBoard)) {
-//                System.out.println("---------------------\n       Solved!       \n---------------------");
-//                return;
-//            }
-//
-//            for (int[] dir: dirMatrix) {
-//                int checkRow = currentBoard.getZeroCoords()[0] + dir[0];
-//                int checkCol = currentBoard.getZeroCoords()[1] + dir[1];
-//                if (isInbounds(currentBoard.getTiles(), checkRow, checkCol)) {
-//                    Board newBoard = new Board(cloneMatrix(currentBoard.getTiles()));
-//                    newBoard.moveZero(dir);
-//
-//                    Vertex v = graph.addVertex(newBoard);
-//                    graph.addEdge(currentVertex, v);
-//                }
-//            }
-//
-//            graph.getAdjVertices(currentVertex).forEach((vertex -> {
-//                if (!vertex.getVisited()) {
-//                    vertex.setVisited(true);
-//                    queue.add(vertex.getIndex());
-//                }
-//            }));
-//        }
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,21 @@
 package src;
 
-import src.Algorithm.Algorithm;
-import src.Algorithm.AlgorithmType;
 import src.Algorithm.BFS;
-
-import java.util.Arrays;
-import java.util.LinkedList;
-
+import static src.Util.generateBoard;
+import static src.Util.createSolvedBoard;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] initialTiles = {
-                {0, 1, 8, 7},
-                {6, 14, 10, 9},
-                {2, 12, 15, 3},
-                {5, 13, 4, 11}
-        };
-        int[][] solvedTiles = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 0}
-        };
 
-        Board initialBoard = new Board(initialTiles);
-        Board solvedBoard = new Board(solvedTiles);
+        String algorithm = "bfs";
+        int size = 4;
+        int n = 10;
 
-        Algorithm bfs = new Algorithm(AlgorithmType.BFS, solvedBoard);
-        Algorithm dijkstra = new Algorithm(AlgorithmType.DIJKSTRA, solvedBoard);
-        Algorithm astar = new Algorithm(AlgorithmType.ASTAR, solvedBoard);
+        Board solvedBoard = createSolvedBoard(size);
+        Board initialBoard = generateBoard(solvedBoard, n);
+        initialBoard.printBoard();
 
-        bfs.solve(initialBoard);
+        BFS bfs = new BFS(initialBoard, solvedBoard);
+        bfs.solve();
     }
 }
